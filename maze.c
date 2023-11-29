6d913411371d621d1ca43232336234eeb940c87b
ThomasPicoZavadil <152439703+ThomasPicoZavadil@users.noreply.github.com>
2023-11-29 21:06:12 +0100

    real maze.c

--- maze.c
+++ maze.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct
 {
@@ -8,105 +9,293 @@ typedef struct
     unsigned char *cells;
 } Map;
 
-void readfile()
+Map *readfile() // dodělat název souboru ze vstupu
 {
-
-    Map map;
+    Map *map = (Map *)malloc(sizeof(Map));
 
     FILE *file = fopen("bludiste.txt", "r");
     if (file == NULL)
     {
-        fprintf(stderr, "Error opening the file.\n");
+        // // fprintf(stderr, "Error opening the file.\n");
+        exit(1);
     }
-    if (fscanf(file, "%d %d", &map.rows, &map.cols) != 2)
+
+    if (fscanf(file, "%d %d", &map->rows, &map->cols) != 2)
     {
-        fprintf(stderr, "Error reading the dimensions.\n");
+        // // fprintf(stderr, "Error reading the dimensions.\n");
         fclose(file);
+        exit(1);
     }
-    map.cells = (unsigned char *)malloc(map.rows * map.cols * sizeof(unsigned char));
-    if (map.cells == NULL)
+
+    map->cells = (unsigned char *)malloc(map->rows * map->cols * sizeof(unsigned char));
+    if (map->cells == NULL)
     {
-        fprintf(stderr, "Memory allocation error.\n");
+        // // fprintf(stderr, "Memory allocation error.\n");
         fclose(file);
+        exit(1);
     }
-    for (int i = 0; i < map.rows * map.cols; i++)
+
+    for (int i = 0; i < map->rows * map->cols; i++)
     {
-        if (fscanf(file, "%hhu", &map.cells[i]) != 1)
+        if (fscanf(file, "%hhu", &map->cells[i]) != 1)
         {
-            fprintf(stderr, "Error reading the matrix elements.\n");
+            // // fprintf(stderr, "Error reading the matrix elements.\n");
             fclose(file);
-            free(map.cells);
-        }
-    }
-    fclose(file);
-}
-
-int main()
-{
-    Map map;
-    readfile();
-    for (int i = 0; i < sizeof(map.cells); i++)
-    {
-        printf("%d", map.cells);
-    }
-    free(map.cells);
-}
-/*
-#include <stdio.h>
-#include <stdlib.h>
-
-    int
-    main()
-{
-    FILE *file = fopen("your_file.txt", "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error opening the file.\n");
-        return 1;
-    }
-
-    int rows, cols;
-
-    // Read the first line
-    if (fscanf(file, "%d %d", &rows, &cols) != 2)
-    {
-        fprintf(stderr, "Error reading the dimensions.\n");
-        fclose(file);
-        return 1;
-    }
-
-    // Allocate memory for the 1D array
-    int *matrix = (int *)malloc(rows * cols * sizeof(int));
-    if (matrix == NULL)
-    {
-        fprintf(stderr, "Memory allocation error.\n");
-        fclose(file);
-        return 1;
-    }
-
-    // Read the rest of the numbers
-    for (int i = 0; i < rows * cols; i++)
-    {
-        if (fscanf(file, "%d", &matrix[i]) != 1)
-        {
-            fprintf(stderr, "Error reading the matrix elements.\n");
-            fclose(file);
-            free(matrix);
-            return 1;
+            free(map->cells);
+            exit(1);
         }
     }
 
-    // Close the file
     fclose(file);
+    return map;
+}
 
-    // Now you have the matrix in the 'matrix' variable
-    // Access elements like matrix[i * cols + j]
+bool isborder(Map *map, int r, int c, int border)
+{
+    return !!(map->cells[r * map->cols + c] & (border == 3 ? 4 : border));
+}
 
-    // Your code here...
+// levá - 1, pravá - 2, horní/dolní - 3
+int start_border(Map *map, int r, int c, int leftright)
+{
+    if (leftright == 0)
+    {
+        if (c == 1)
+        {
+            if (r % 2 == 1 && isborder(map, r - 1, c - 1, 1) == false)
+            {
+                // // fprintf(stdout, "PICA1\n");
+                printf("%d, %d\n", r, c);
+                return 2;
+            }
+            if (r % 2 == 0 && isborder(map, r - 1, c - 1, 1) == false)
+            {
+                // // fprintf(stdout, "PICA2\n");
+                printf("%d, %d\n", r, c);
+                return 3;
+            }
+        }
+        if (c == map->cols)
+        {
+            if (r % 2 == 1 && isborder(map, r - 1, c - 1, 2) == false)
+            {
+                // // fprintf(stdout, "PICA3\n");
+                printf("%d, %d\n", r, c);
+                return 3;
+            }
+            if (r % 2 == 0 && isborder(map, r - 1, c - 1, 2) == false)
+            {
+                // // fprintf(stdout, "PICA4\n");
+                printf("%d, %d\n", r, c);
+                return 1;
+            }
+        }
+        if (r == 1 && c % 2 == 1)
+        {
+            if (isborder(map, r - 1, c - 1, 3) == false)
+            {
+                // // fprintf(stdout, "PICA5\n");
+                printf("%d, %d\n", r, c);
+                return 1;
+            }
+        }
+        if (r == map->rows)
+        {
+            if (r % 2 == 0 && c % 2 == 1 && isborder(map, r - 1, c - 1, 3) == false)
+            {
+                // // fprintf(stdout, "PICA6\n");
+                printf("%d, %d\n", r, c);
+                return 2;
+            }
+            if (r % 2 == 1 && c % 2 == 0 && isborder(map, r - 1, c - 1, 3) == false)
+            {
+                // // fprintf(stdout, "PICA7\n");
+                printf("%d, %d\n", r, c);
+                return 2;
+            }
+        }
+        return 0;
+    }
+    return 0;
+}
 
-    // Don't forget to free the allocated memory when done
-    free(matrix);
+void r_path(Map *map, int r, int c, int border)
+{
+    int lastborder = border;
+    while (true)
+    {
+        if (isborder(map, r - 1, c - 1, border) == false)
+        {
+            if (border == 1)
+            {
+                // // fprintf(stdout, "BEER1\n");
+                c--;
+                if ((r + c) % 2 == 0)
+                {
+                    // fprintf(stdout, "BEER2\n");
+                    border = 3;
+                    lastborder = border;
+                    if (r < 1 || r > map->rows || c < 1 || c > map->cols)
+                    {
+                        break;
+                    }
+                    printf("%d, %d\n", r, c);
+                }
+                else
+                {
+                    // fprintf(stdout, "BEER3\n");
+                    border = 1;
+                    lastborder = border;
+                    if (r < 1 || r > map->rows || c < 1 || c > map->cols)
+                    {
+                        break;
+                    }
+                    printf("%d, %d\n", r, c);
+                }
+            }
+            if (border == 2)
+            {
+                // // fprintf(stdout, "BEER4\n");
+                c++;
+                if ((r + c) % 2 == 0)
+                {
+                    border = 2;
+                    lastborder = border;
+                    // fprintf(stdout, "BEER5\n");
+                    if (r < 1 || r > map->rows || c < 1 || c > map->cols)
+                    {
+                        break;
+                    }
+                    printf("%d, %d\n", r, c);
+                }
+                else
+                {
+                    border = 3;
+                    lastborder = border;
+                    // fprintf(stdout, "BEER6\n");
+                    if (r < 1 || r > map->rows || c < 1 || c > map->cols)
+                    {
+                        break;
+                    }
+                    printf("%d, %d\n", r, c);
+                }
+            }
+            if (border == 3)
+            {
+                // // fprintf(stdout, "BEER7\n");
+                if ((r + c) % 2 == 0)
+                {
+                    // // fprintf(stdout, "BEER8\n");
+                    r--;
+                }
+                else
+                {
+                    // // fprintf(stdout, "BEER9\n");
+                    r++;
+                }
+                if ((r + c) % 2 == 0)
+                {
+                    // fprintf(stdout, "BEER_10\n");
+                    border = 1;
+                    lastborder = border;
+                    if (r < 1 || r > map->rows || c < 1 || c > map->cols)
+                    {
+                        break;
+                    }
+                    printf("%d, %d\n", r, c);
+                }
+                else
+                {
+                    // fprintf(stdout, "BEER_11\n");
+                    border = 2;
+                    lastborder = border;
+                    if (r < 1 || r > map->rows || c < 1 || c > map->cols)
+                    {
+                        break;
+                    }
+                    printf("%d, %d\n", r, c);
+                }
+            }
+        }
+        else
+        {
+            if (lastborder == 1)
+            {
+                if ((r + c) % 2 == 1)
+                {
+                    // fprintf(stdout, "Vlevo stena 1\n");
+                    border = 3;
+                    lastborder = border;
+                }
+                else
+                {
+                    // fprintf(stdout, "Vlevo stena 2\n");
+                    border = 2;
+                    lastborder = border;
+                }
+            }
+            if (lastborder == 2)
+            {
+                if ((r + c) % 2 == 1)
+                {
+                    // fprintf(stdout, "Vpravo stena 1\n");
+                    border = 1;
+                    lastborder = border;
+                }
+                else
+                {
+                    // fprintf(stdout, "Vpravo stena 2\n");
+                    border = 3;
+                    lastborder = border;
+                }
+            }
+            if (lastborder == 3)
+            {
+                if ((r + c) % 2 == 1)
+                {
+                    // fprintf(stdout, "Nahore/dole stena 1\n");
+                    border = 2;
+                    lastborder = border;
+                }
+                else
+                {
+                    // fprintf(stdout, "Nahore/dole stena 2\n");
+                    border = 1;
+                    lastborder = border;
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    Map *map = readfile();
+    /* for (int i = 0; i < map->cols * map->rows; i++)
+     {
+         printf("%d ", map->cells[i]);
+     }*/
+    if (argc > 5)
+    {
+        // // fprintf(stderr, "Picovina");
+        return 1;
+    }
+    int r = atoi(argv[1]);
+    int c = atoi(argv[2]);
+    int border = start_border(map, r, c, 0);
+
+    r_path(map, r, c, border);
+    /*
+        for (int i = 0; i < map->rows; i++)
+        {
+            for (int j = 0; j < map->cols; j++)
+            {
+                printf("%hhu ", map->cells[i * map->cols + j]);
+            }
+            printf("\n");
+        }
+    */
+    free(map->cells);
 
     return 0;
 }
-*/
